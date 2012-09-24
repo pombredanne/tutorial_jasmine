039a617fa2a59b171d82beb67349d5be8fcf3295
Mark Fink <mark@mark-fink.de>
2012-09-24 11:55:41 +0200

    config change

--- tutorialFiles.js
+++ tutorialFiles.js
@@ -15,31 +15,12 @@ tutorialFiles = {
 if (typeof JASMINE_ADAPTER !== 'undefined') {
   // Testacular config
   var mergedFiles = [];
-  angularFiles.jstd.forEach(function(file) {
-    // replace @ref
-    var match = file.match(/^\@(.*)/);
-    if (match) {
-      var deps = angularFiles[match[1]];
-      if (!deps) {
-        console.log('No dependency:' + file)
-      }
-      mergedFiles = mergedFiles.concat(deps);
-    } else {
-      mergedFiles.push(file);
-    }
-  });
 
   files = [JASMINE, JASMINE_ADAPTER];
 
-  mergedFiles.forEach(function(file){
-    if (/jstd|jasmine/.test(file)) return;
-    files.push(file);
-  });
-
-
   exclude = angularFiles.jstdExclude;
 
-  autoWatch = true;
+  autoWatch = false;
   autoWatchInterval = 1;
   logLevel = LOG_INFO;
   logColors = true;
